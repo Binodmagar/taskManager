0d14eb1caf40ba2411ed585d4148006b661b978c
Binodmagar <ranabinod615@gmail.com>
2019-12-31 08:52:18 +0545

    StrictMode, URl, taksmanager, signup.xml file done

---
+++ app/src/main/java/com/binod/URL/Url.java
@@ -0,0 +1,26 @@
+package com.binod.URL;
+
+import java.net.URI;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Url {
+
+    public static final String base_url = "http://172.100.100.5:3000/";
+//    public static final String base_url = "http://10.0.2.2:3000/";
+////    public static final String base_url = "http://192.168.1.11:3000/";
+
+    public static  String token = "";
+
+    public static Retrofit CreateInstance(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Url.base_url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return  retrofit;
+    }
+
+
+}
